a60ca3d63a6adf284b393788ef030fa4c166a0ba
jessica alarcon cardona <jessica.alarcon9124@gmail.com>
2017-11-02 16:58:04 -0500

    Maquina de turing

---
+++ MT/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace MT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::MT.Properties.Resources.Equis;
+            this.pictureBox2.Location = new System.Drawing.Point(12, 286);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(47, 45);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+           
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 120);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ingrese la cadena";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 161);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "JUGAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(432, 362);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ MT/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MT
+{
+    public partial class Form1 : Form
+    {
+        public PictureBox[] imagenes = new PictureBox[121];
+        public int count = 0;
+        public int modulo = 0;
+        public int tamaño = 0;
+        public string w;
+
+        public Form1()
+        {
+
+            InitializeComponent();
+            CrearMatriz(11);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void CrearMatriz(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    System.Windows.Forms.PictureBox pictureBox = new System.Windows.Forms.PictureBox(); ;
+                    ((System.ComponentModel.ISupportInitialize)(pictureBox)).BeginInit();
+                    pictureBox.Location = new System.Drawing.Point(300 + (j * 47), 50 + (i * 47));
+                    pictureBox.Size = new System.Drawing.Size(47, 47);
+                    pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+                    pictureBox.TabStop = false;
+                    Controls.Add(pictureBox);
+                    imagenes[(i * 11) + j] = pictureBox;
+
+                }
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (count < tamaño * tamaño)
+            {
+                if (textBox2.Text[count].Equals('x') || textBox2.Text[count].Equals('X'))
+                {
+                    imagenes[(count % tamaño) + (modulo * 11)].Image = global::MT.Properties.Resources.Equis;
+                    count++;
+                }
+                else if (textBox2.Text[count].Equals('o') || textBox2.Text[count].Equals('O'))
+                {
+                    imagenes[(count % tamaño) + (modulo * 11)].Image = global::MT.Properties.Resources.Circulo;
+                    count++;
+                }
+                if (count % tamaño == 0)
+                {
+                    modulo += 1;
+                }
+            }
+        }
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            descartarletra(e);
+        }
+        private void descartarletra(KeyPressEventArgs e)
+        {
+
+            if (Char.IsDigit(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else
+            if (Char.IsControl(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+
+        }
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            if(textBox1.Text != "" && Convert.ToInt32(textBox1.Text) >= 3)
+            {
+                
+                tamaño = Convert.ToInt32(textBox1.Text);
+
+            }
+            else
+            {
+                MessageBox.Show("Debe ingresar un número mayor a 3");
+            }
+
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (count < tamaño * tamaño)
+            {
+                MessageBox.Show("Debe ingresar todos los valores, faltan : " + ((tamaño * tamaño) - count));
+            }
+            else
+            {
+                w = textBox2.Text;
+                ValidarHorizontal(w, tamaño);
+            }
+        }
+        public void ValidarHorizontal(string w, int tamaño)
+        {
+            int ganadorO = tamaño;
+            int ganadorX = tamaño;
+            for (int i = 0; i < tamaño; i++)
+            {
+                if (w[i * tamaño].Equals('x'))
+                {
+                    ganadorO -= 1;
+                }
+                else
+                {
+                    ganadorX -= 1;
+                }
+                for (int j = 1; j < tamaño; j++)
+                {
+                    if (!w[((i * tamaño) + j) - 1].Equals(w[((i * tamaño) + j)]))
+                    {
+                        ganadorO -= 1;
+                        ganadorX -= 1;
+                        break;
+                    }
+
+                }
+            }
+            MessageBox.Show("x: " + ganadorX + "     O: " + ganadorO);
+        }
+    }
+}
